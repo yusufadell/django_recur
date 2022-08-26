1c93310470a0f32d3e87de0755df570ee0cc96c9
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-08-26 20:58:31 +0200

    feat: dad logic for retrieving issues list and get one details

---
+++ newsfeed/views.py
@@ -0,0 +1,38 @@
+from django.views.generic import ListView
+from django.views.generic.detail import SingleObjectMixin
+
+from .models import Issue, Post
+
+
+class IssueListView(ListView):
+    model = Issue
+    template_name = "newsfeed/issue_list.html"
+
+    def get_queryset(self):
+        return super().get_queryset().released()
+
+
+issue_list_view = IssueListView.as_view()
+
+
+class IssueDetailView(SingleObjectMixin, ListView):
+    model = Post
+    paginate_by = 20
+    slug_url_kwarg = "issue_number"
+    slug_field = "issue_number"
+    template_name = "newsfeed/issue_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Issue.objects.released())
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["issue"] = self.object
+        return context
+
+    def get_queryset(self):
+        return self.object.posts.filter(is_visible=True).select_related("category")
+
+
+issue_detail_view = IssueDetailView.as_view()
