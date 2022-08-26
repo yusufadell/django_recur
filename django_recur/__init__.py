8e85476651a200f149735ad627ebfefd854b3b67
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-08-26 18:06:12 +0200

    🎉 start django-recur

--- django_recur/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for django-recur."""
-
-__author__ = """Yusuf Adel"""
-__email__ = 'yusufadell.dev@gmail.com'
-__version__ = '0.1.0'
--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use django-recur in a project::
-
-    import django_recur
